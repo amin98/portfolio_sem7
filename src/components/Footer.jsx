9381192d0edb4633d59dad1f2664dd5404146861
Amin <499888@student.fontys.nl>
2025-04-08 12:37:39 +0200

    fontys commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,7 @@ export default function Footer() {
     <footer className="bg-gray-50 border-t">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="mt-8 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between">
-          <div className="flex space-x-6 md:order-2">
-            <a href="mailto:contact@example.com" className="text-gray-400 hover:text-gray-500">
-              contact@example.com
-            </a>
-          </div>
+       
           <p className="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
             © {new Date().getFullYear()} Portfolio. All rights reserved.
           </p>
